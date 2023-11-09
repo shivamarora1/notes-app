8a1ab6cf3a8ff79db1a0cdb3ce195220cda25f0d
Shivam Arora <shivamarora1@yahoo.com>
2023-11-09 20:35:07 +0530

    added style in website basic

--- app/ui/note.tsx
+++ app/ui/note.tsx
@@ -1,7 +1,7 @@
 export default function Note({ note }: { note: string }) {
   return (
-    <div>
-      <p>{note}</p>
+    <div className="overflow-hidden hover:overflow-y-auto w-80 h-80 bg-blue-200 p-4 my-4 mx-6 rounded-md">
+      <p className="text-blue-800">{note}</p>
     </div>
   );
 }
--- app/ui/notes-list.tsx
+++ app/ui/notes-list.tsx
@@ -1,7 +1,7 @@
 import Note from "@/app/ui/note";
 export default function NotesList({ notes }: { notes: string[] }) {
   return (
-    <div>
+    <div className="flex flex-wrap mx-3 my-3">
       {notes.map((note, idx) => (
         <Note key={idx} note={note} />
       ))}
